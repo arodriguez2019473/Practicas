dff5d450c7460eb44f1dedd3c326e23ee9062bb4
Auky Rodriguez <2473@gruporeal.ad>
2024-10-07 16:31:32 -0600

    se hace otra subida

--- python-flask/endpoind1.py
+++ python-flask/endpoind1.py
@@ -6,5 +6,15 @@ app = Flask(__name__)
 port = 5000
 
 response = requests.get("https://api.chucknorris.io/jokes/random")
-   
 
+
+data = response.json()
+
+
+@app.route('/data', methods=['GET'])
+def test():
+
+    return data,200
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', debug=True, port=port)
