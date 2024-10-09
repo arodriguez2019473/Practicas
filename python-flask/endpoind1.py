f6b4a167df2b82951909bbb934159a3681d3d368
Auky Rodriguez <2473@gruporeal.ad>
2024-10-09 14:35:00 -0600

    se hace primera subida en hora y media antes

--- python-flask/endpoind1.py
+++ python-flask/endpoind1.py
@@ -6,26 +6,21 @@ app = Flask(__name__)
 port = 5000
 @app.route('/')
 
-
 @app.route('/data', methods=['GET'])
 
 def test():
     a = []
     b = set()
-
-    for i in range(1):
+    rango = 3
+    while len(b) < rango: 
         response = requests.get("https://api.chucknorris.io/jokes/random")
-    
-        if response.status_code == 200:     
+        if response.status_code == 200:
                 data = response.json()
-                # a.append(data) // si se agrega esto puedo duplicar 2 veces el endpoint 
-                jokeId = data['id']
-
-                if jokeId not in a:
-                    a.append(data)  
-                
-                b.add(jokeId)
-
+                jokeId:str = data['id']
+                if not jokeId in b:
+                    b.add(jokeId)
+                    a.append(data)
+                    
     return jsonify(a),200
 
 if __name__ == '__main__':
