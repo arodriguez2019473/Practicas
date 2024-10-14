d56897622c58614fe14c05e80cc0914fb959c87d
Auky Rodriguez <2473@gruporeal.ad>
2024-10-14 14:52:27 -0600

    se hace  un push antes de la hora de salida

--- PokeApi/backend/end.py
+++ PokeApi/backend/end.py
@@ -1,3 +1,4 @@
+
 from flask import Flask, jsonify, request
 import requests
 import uuid
@@ -9,39 +10,22 @@ URL = "https://pokeapi.co/api/v2/pokemon"
 
 @app.route('/pokens', methods=['GET'])
 def getpokes():
-
     response = requests.get(URL)
     data = response.json()
 
-    # Extraigas una lista de 10 pokemones
-    # luego devolvas la lsita de los 10 pokemones
-    
     queryparams = request.args
-    limit = queryparams.get('limit',default=10, type=int)
-    results = data['results'][0:limit:1]
-# [1:limit:1]
+    limit = queryparams.get('limit', default=10, type=int)
 
-    # luego extraigas cada uno de los datos de los pokemones
+    results = data['results'][0:limit]
 
-    detalles = results['url']
+    pokemon_details = []
+    for result in results:
+        pokemon_url = result['url']
+        response2 = requests.get(pokemon_url)
+        pokemon_data = response2.json()
+        pokemon_details.append(pokemon_data)
 
-    # response1 = requests.get(details)
-
-    # data1 = response1.json()
-
-    # print(data1)
-
-    return jsonify(results), 200
+    return jsonify({'results': results, 'details': pokemon_details}), 200
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', debug=True, port=port)  
-
-
-@app.route('/details', methods=['GET'])
-def getdetails():
-
-    response = requests.get(URL)
-    data = response.json()
-    results = data['url']
-
-    return jsonify(results), 200
+    app.run(host='0.0.0.0', debug=True, port=port)
