75bc17f20cf4e8595ca398e298b95c88237b325f
Auky Rodriguez <2473@gruporeal.ad>
2024-10-18 16:02:27 -0600

    se hace push

--- PokeApi/fronend/src/app/pages/home/home.component.ts
+++ PokeApi/fronend/src/app/pages/home/home.component.ts
@@ -1,16 +1,18 @@
 import { Component, OnInit } from '@angular/core';
 import { ServiceService } from '../../service.service';
 import { RouterModule } from '@angular/router';
-
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faCoffee } from '@fortawesome/free-solid-svg-icons';
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [RouterModule],
+  imports: [RouterModule,FontAwesomeModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 export class HomeComponent implements OnInit {
   data: any;
+  faCoffee = faCoffee;
 
   constructor(
     private pokensSvc: ServiceService
